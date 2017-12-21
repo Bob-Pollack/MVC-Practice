d6173bfb0dd44bf72328eddcf56aa40f04bb7113
Bob-Pollack <bobpollack@gmail.com>
2017-12-21 09:18:15 -0500

    adding website test to repository

---
+++ WebsiteTestAlpha/WebsiteTestAlpha/Controllers/AlphaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteTestAlpha.Models;
+
+namespace WebsiteTestAlpha.Controllers
+{
+    public class AlphaController : Controller
+    {
+        // GET: Alpha
+        public ActionResult Index()
+        {
+            AlphaModel alphys = new AlphaModel();
+
+            alphys.CountUp = 0;
+
+            alphys.UserInput = 0;
+
+            alphys.ExtraMessage = "test";
+
+            return View("Index", alphys);
+        }
+
+        public ActionResult RepeatProcedure(AlphaModel a)
+        {
+            AlphaModel alphys = new AlphaModel();
+
+            alphys.CountUp = a.CountUp + 1;
+
+            alphys.UserInput = a.UserInput;
+
+            alphys.ExtraMessage = a.ExtraMessage;
+
+            return View("Index", alphys);
+            //a.CountUp = a.CountUp + 1 ;
+
+            //return View("Index", a);
+        }
+    }
+}
---
+++ WebsiteTestAlpha/WebsiteTestAlpha/Models/AlphaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteTestAlpha.Models
+{
+    public class AlphaModel
+    {
+        public int CountUp { get; set; }
+
+        [Range(-100, 100, ErrorMessage = "absolute value of range must be 100 or less")]
+        public double UserInput { get;  set;}
+
+        public string ExtraMessage { get; set; }
+        
+
+        
+
+    }
+}
